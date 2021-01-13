71d6bf8c503395c25d9d008442d78628ff11d00d
Jonathan Sick <jonathan@jsick.codes>
2021-01-13 18:46:02 -0500

    Parse available metadata from pipeline doc
    This adds parsing support for:
    
    - version
    - authors (including IPAC and SPHEREx lead roles)
    - pipeline level
    
    Includes a custom subclass of DocumentMetadata to provide support for
    the pipeline_level metadata attribute, as well as convenience accessors
    for the different types of authors.

--- src/spherexlander/parsers/pipelinemodule/__init__.py
+++ src/spherexlander/parsers/pipelinemodule/__init__.py
@@ -1,7 +1,10 @@
 """Parser for SPHEREx pipeline modoule documents."""
 
-__all__ = ["SpherexPipelineModuleParser"]
+__all__ = ["SpherexPipelineModuleParser", "SpherexPipelineModuleMetadata"]
 
+from spherexlander.parsers.pipelinemodule.datamodel import (
+    SpherexPipelineModuleMetadata,
+)
 from spherexlander.parsers.pipelinemodule.parser import (
     SpherexPipelineModuleParser,
 )
--- tests/test_parser_pipelinemodule.py
+++ tests/test_parser_pipelinemodule.py
@@ -15,4 +15,18 @@ def test_demodoc() -> None:
 
     parser = SpherexPipelineModuleParser(root_tex_path)
 
-    assert parser.metadata.title == "Perform Forced Photometry"
+    m = parser.metadata
+    assert m.title == "Perform Forced Photometry"
+    assert m.version == "1.1"
+    assert m.pipeline_level == "L3"
+
+    assert len(m.authors) == 4
+    assert len(m.other_authors) == 2
+    assert m.ipac_lead.name == "Francis Carrillo"
+    assert m.ipac_lead.email == "francis@example.com"
+    assert m.spherex_lead.name == "Ursula Gomez"
+    assert m.spherex_lead.email == "ursula@example.com"
+    assert m.authors[2].name == "Stephanie Lowe"
+    assert m.authors[2].email == "stephanie@example.com"
+    assert m.authors[3].name == "Efren Archer"
+    assert m.authors[3].email == "efren@example.com"
