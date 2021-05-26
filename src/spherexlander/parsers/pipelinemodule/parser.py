f7a53f6ea4ba722b4e7998ab0de82d9e247fbcce
Jonathan Sick <jonathan@jsick.codes>
2021-05-26 11:55:32 -0400

    Update parsing for spherex class
    This update to the parsing works with the new spherex latex class in
    https://github.com/SPHEREx/spherex-tex and its metadata commands such as
    \difficulty, \diagramindex, \spherexlead, \ipaclead, \author, and
    \person.

--- src/spherexlander/parsers/pipelinemodule/parser.py
+++ src/spherexlander/parsers/pipelinemodule/parser.py
@@ -2,14 +2,19 @@
 
 from __future__ import annotations
 
-import re
+from collections import UserDict
 from logging import getLogger
 from typing import Any, Dict, List, Optional
 
 from lander.ext.parser import CiPlatform, Contributor, Parser
 from lander.ext.parser.pandoc import convert_text
+from lander.ext.parser.texutils.extract import (
+    LaTeXCommand,
+    LaTeXCommandElement,
+)
 
 from spherexlander.parsers.pipelinemodule.datamodel import (
+    Difficulty,
     SpherexPipelineModuleMetadata,
 )
 
@@ -27,6 +32,8 @@ class SpherexPipelineModuleParser(Parser):
             "title": self._parse_module_name(),
             "version": self._parse_version(),
             "pipeline_level": self._parse_pipeline_level(),
+            "difficulty": self._parse_difficulty(),
+            "diagram_index": self._parse_diagram_index(),
             "authors": self._parse_authors(),
         }
 
@@ -47,91 +54,151 @@ class SpherexPipelineModuleParser(Parser):
 
     def _parse_module_name(self) -> str:
         """Parse the pipeline module name."""
-        try:
-            name_content = self.tex_macros[r"\modulename"]
-            name = convert_text(
-                content=name_content, source_fmt="latex", output_fmt="plain"
-            )
-        except KeyError:
-            logger.warning("No modulename command detected")
-            return ""
-
-        return name
+        command = LaTeXCommand(
+            "modulename",
+            LaTeXCommandElement(name="name", required=True, bracket="{"),
+        )
+        names = [n for n in command.parse(self.tex_source)]
+        if len(names) == 0:
+            raise RuntimeError("Could not parse a modulename command.")
+        return names[-1]["name"]
 
     def _parse_version(self) -> Optional[str]:
         """Parse the version command."""
-        try:
-            version = self.tex_macros[r"\version"]
-        except KeyError:
+        command = LaTeXCommand(
+            "version",
+            LaTeXCommandElement(name="version", required=True, bracket="{"),
+        )
+        versions = [v for v in command.parse(self.tex_source)]
+        if len(versions) == 0:
             logger.warning("No version command detected")
             return None
-
-        return version
+        return versions[-1]["version"]
 
     def _parse_pipeline_level(self) -> Optional[str]:
         """Parse the pipelevel command."""
-        try:
-            pipeline_level = self.tex_macros[r"\pipelevel"]
-        except KeyError:
+        command = LaTeXCommand(
+            "pipelevel",
+            LaTeXCommandElement(name="level", required=True, bracket="{"),
+        )
+        levels = [i for i in command.parse(self.tex_source)]
+        if len(levels) == 0:
             logger.warning("No pipelevel command detected")
-            return ""
+            return None
+        return levels[-1]["level"]
 
-        return pipeline_level
+    def _parse_difficulty(self) -> Difficulty:
+        """Parse the difficulty command."""
+        command = LaTeXCommand(
+            "difficulty",
+            LaTeXCommandElement(name="difficulty", required=True, bracket="{"),
+        )
+        difficulties = [d for d in command.parse(self.tex_source)]
+        if len(difficulties) == 0:
+            logger.warning("No difficulty command detected")
+            return Difficulty.Unassigned
+
+        value = difficulties[-1]["difficulty"]
+        try:
+            return Difficulty[value]
+        except Exception:
+            logger.warning(
+                "Difficulty value %s is not one of: %s",
+                value,
+                str([d.name for d in Difficulty]),
+            )
+            return Difficulty.Unassigned
+
+    def _parse_diagram_index(self) -> Optional[int]:
+        """Parse the diagramindex command."""
+        command = LaTeXCommand(
+            "diagramindex",
+            LaTeXCommandElement(name="di", required=True, bracket="{"),
+        )
+        diagramindices = [di for di in command.parse(self.tex_source)]
+        if len(diagramindices) == 0:
+            logger.warning("No diagramindex command detected")
+            return None
+
+        return int(diagramindices[-1]["di"])
 
     def _parse_authors(self) -> List[Contributor]:
         """Parse author information, including lead and other authors."""
         authors: List[Contributor] = []
 
-        ipac_match = re.search(
-            # Because of normalization, "\ipac\" becomes "IPAC"
-            r"IPAC Lead:\s+(?P<author>\S[A-Za-z@\.\s]+)\}",
-            self.tex_source,
-        )
-        if ipac_match:
-            authors.append(
-                self._split_author_info(ipac_match["author"], role="IPAC Lead")
-            )
-        else:
-            logger.warning("Did not detect IPAC lead")
+        spherex_lead = self._parse_lead(kind="spherex", role="SPHEREx Lead")
+        if spherex_lead:
+            authors.append(spherex_lead)
 
-        spherex_match = re.search(
-            # Because of normalization, "\ipac\" becomes "IPAC"
-            r"SPHEREx Lead:\s+(?P<author>\S[A-Za-z@\.\s]+)\}",
-            self.tex_source,
-        )
-        if spherex_match:
-            authors.append(
-                self._split_author_info(
-                    spherex_match["author"], role="SPHEREx Lead"
-                )
-            )
-        else:
-            logger.warning("Did not detect SPHEREx lead")
+        ipac_lead = self._parse_lead(kind="ipac", role="IPAC Lead")
+        if ipac_lead:
+            authors.append(ipac_lead)
 
-        # Parsing "other authors"
-        # Sample in the source:
-        #
-        # {\noindent\bf Other Authors:}\\
-        # Stephanie Lowe stephanie@example.com\\
-        # Efren Archer efren@example.com\\
-        #
-        other_match = re.search(
-            r"Other Authors:[\{\}\s]+\\\\\n"
-            r"(?P<authors>((\S[A-Za-z\s@\.]+)\\\\\n)+)",
-            self.tex_source,
+        author_command = LaTeXCommand(
+            "author",
+            LaTeXCommandElement(name="body", required=True, bracket="{"),
         )
-        if other_match:
-            for author_info in other_match["authors"].strip().split("\n"):
-                author_info = author_info.rstrip(r"\\")
-                authors.append(self._split_author_info(author_info))
+        person_command = LaTeXCommand(
+            "person",
+            LaTeXCommandElement(name="options", required=False, bracket="["),
+            LaTeXCommandElement(name="name", required=True, bracket="{"),
+        )
+
+        for author_instance in author_command.parse(self.tex_source):
+            for person_instance in person_command.parse(
+                author_instance["body"]
+            ):
+                name = convert_tex_span(person_instance["name"])
+                if "options" in person_instance:
+                    option_map = KVOptionMap.parse(person_instance["options"])
+                else:
+                    option_map = KVOptionMap()
+                if "email" in option_map:
+                    email = option_map["email"]
+                else:
+                    email = None
+                authors.append(Contributor(name=name, email=email))
 
         return authors
 
-    def _split_author_info(
-        self, author_string: str, role: Optional[str] = None
-    ) -> Contributor:
-        """Extract the author name and email into a Contributor object."""
-        parts = author_string.strip().split()
-        name = " ".join(parts[:-1])
-        email = parts[-1]
+    def _parse_lead(self, *, kind: str, role: str) -> Optional[Contributor]:
+        command_name = f"{kind}lead"
+        command = LaTeXCommand(
+            command_name,
+            LaTeXCommandElement(name="options", required=False, bracket="["),
+            LaTeXCommandElement(name="name", required=True, bracket="{"),
+        )
+        instances = [i for i in command.parse(self.tex_source)]
+        if len(instances) == 0:
+            logger.warning("No %s command detected", command_name)
+            return None
+
+        instance = instances[-1]
+        name = convert_tex_span(instance["name"])
+        if "options" in instance:
+            option_map = KVOptionMap.parse(instance["options"])
+        else:
+            option_map = KVOptionMap()
+        if "email" in option_map:
+            email = option_map["email"]
+        else:
+            email = None
         return Contributor(name=name, email=email, role=role)
+
+
+class KVOptionMap(UserDict):
+    @classmethod
+    def parse(self, options: str) -> KVOptionMap:
+        keyvalues = [kv.strip() for kv in options.split(",")]
+        split_keyvalues = [tuple(kv.split("=")[:2]) for kv in keyvalues]
+        # A work-around for type checking; could be improved
+        return KVOptionMap({k: v for k, v in split_keyvalues})
+
+
+def convert_tex_span(content: str) -> str:
+    return convert_text(
+        content=content,
+        source_fmt="latex",
+        output_fmt="plain",
+        deparagraph=True,
+    )
