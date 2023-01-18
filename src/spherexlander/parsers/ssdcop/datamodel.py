8a09435a99ba9f7b80a5d0944ac6298b9236da27
Jonathan Sick <jonathan@jsick.codes>
2023-01-18 14:37:08 -0500

    Add spherex-ssdc-tn and spherex-ssdc-op parsers
    These parsing plugins support SSDC-OP and SSDC-TN documents.

---
+++ src/spherexlander/parsers/ssdcop/datamodel.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from typing import List, Optional
+
+from lander.ext.parser import Contributor
+
+from ..spherexdata import ApprovalInfo, SpherexMetadata
+
+__all__ = ["SpherexSsdcOpMetadata"]
+
+
+class SpherexSsdcOpMetadata(SpherexMetadata):
+    """Metadata container for describing SSDC-OP documents.
+
+    This metadata is gathered from the content of the document as well as from
+    configuration files provided during the build. This metadata is used to
+    populate the landing page.
+    """
+
+    approval: Optional[ApprovalInfo] = None
+
+    @property
+    def spherex_lead(self) -> Optional[Contributor]:
+        """The lead SPHEREx author."""
+        for author in self.authors:
+            if author.role == "SPHEREx Lead":
+                return author
+        return None
+
+    @property
+    def other_authors(self) -> List[Contributor]:
+        """Additional authors."""
+        return [a for a in self.authors if a.role not in {"SPHEREx Lead"}]
