b42ecf7d93fa9cfbbc9c45aa595f44a02c1fb918
Jonathan Sick <jonathan@jsick.codes>
2023-01-18 13:59:38 -0500

    Add DOORS ID and Jira support for SSDC-TR docs
    Includes test documents:
    
    - tests/data/ssdc-tr-req
    - tests/data/ssdc-tr-va

--- src/spherexlander/parsers/ssdctr/__init__.py
+++ src/spherexlander/parsers/ssdctr/__init__.py
@@ -1,6 +1,9 @@
 """Parser for SPHEREx SSDC-TR documents."""
 
-from spherexlander.parsers.ssdctr.datamodel import SpherexSsdcTrMetadata
+from spherexlander.parsers.ssdctr.datamodel import (
+    DoorsID,
+    SpherexSsdcTrMetadata,
+)
 from spherexlander.parsers.ssdctr.parser import SpherexSsdcTrParser
 
-__all__ = ["SpherexSsdcTrMetadata", "SpherexSsdcTrParser"]
+__all__ = ["SpherexSsdcTrMetadata", "DoorsID", "SpherexSsdcTrParser"]
--- src/spherexlander/parsers/ssdctr/datamodel.py
+++ src/spherexlander/parsers/ssdctr/datamodel.py
@@ -3,10 +3,19 @@ from __future__ import annotations
 from typing import List, Optional
 
 from lander.ext.parser import Contributor
+from pydantic import BaseModel, Field
 
 from ..spherexdata import ApprovalInfo, SpherexMetadata
 
 __all__ = ["SpherexSsdcTrMetadata"]
+
+
+class DoorsID(BaseModel):
+    """A model for a Doors ID (either VA or Req)"""
+
+    id: str = Field(..., description="Doors ID")
+
+    url: str = Field(..., description="Doors URL")
 
 
 class SpherexSsdcTrMetadata(SpherexMetadata):
@@ -18,6 +27,18 @@ class SpherexSsdcTrMetadata(SpherexMetadata):
     """
 
     approval: Optional[ApprovalInfo] = None
+
+    ipac_jira_id: Optional[str] = Field(
+        None, description="IPAC Jira issue ID for test report."
+    )
+
+    va_doors_id: Optional[DoorsID] = Field(
+        None, description="VA (verification activity) Doors ID"
+    )
+
+    req_doors_id: Optional[DoorsID] = Field(
+        None, description="REQ (requirement) Doors ID"
+    )
 
     @property
     def spherex_lead(self) -> Optional[Contributor]:
