1a815d295e3e1363fe55f6a410cac16ef924947c
Jonathan Sick <jonathan@jsick.codes>
2024-07-17 17:08:27 -0400

    Upgrade to lander 2.0.0a11
    Lander 2.0.0a11 is only compatible with Python 3.11 and 3.12, so update
    the version requirement and testing matrix as well.

--- src/spherexlander/__init__.py
+++ src/spherexlander/__init__.py
@@ -3,13 +3,7 @@
 
 __all__ = ["__version__"]
 
-import sys
-
-if sys.version_info < (3, 8):
-    from importlib_metadata import PackageNotFoundError, version
-else:
-    from importlib.metadata import PackageNotFoundError, version
-
+from importlib.metadata import PackageNotFoundError, version
 
 __version__: str
 """The package version string (PEP 440 / SemVer compatible)."""
